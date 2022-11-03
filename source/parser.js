22c8688904851fb64387376cd3c061eeefee7da2
samheckle <samheckle@yahoo.com>
2022-11-03 16:29:02 -0400

    added oct

--- source/parser.js
+++ source/parser.js
@@ -1,22 +1,25 @@
 // get all iframes that were parsed before this tag
-var iframes = document.getElementsByTagName("iframe");
+window.addEventListener("load", (event) => {
+  var iframes = document.getElementsByTagName("iframe");
+  console.log(iframes.length);
 
-for (let i = 0; i < iframes.length; i++) {
+  for (let i = 0; i < iframes.length; i++) {
     var url = iframes[i].getAttribute("src");
     if (url.startsWith("https://docs.google.com/document/d/")) {
-        // create div and replace iframe
-        let d = document.createElement('div');
-        d.classList.add("post-container"); // optional
-        iframes[i].parentElement.replaceChild(d, iframes[i]);
+      // create div and replace iframe
+      let d = document.createElement("div");
+      d.classList.add("post-container"); // optional
+      iframes[i].parentElement.replaceChild(d, iframes[i]);
 
-        // CORS request
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
-        xhr.onload = function() {
-            // display response
-            d.innerHTML = xhr.responseText;
-            d.style.fontSize = '.4em'
-        };
-        xhr.send();
+      // CORS request
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", url, true);
+      xhr.onload = function () {
+        // display response
+        d.innerHTML = xhr.responseText;
+        d.style.fontSize = ".4em";
+      };
+      xhr.send();
     }
-}
+  }
+});
